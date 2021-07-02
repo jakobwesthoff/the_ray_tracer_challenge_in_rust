c0c13a0b1ebeafb4bca0692d66359c3d51551bec
Jakob Westhoff <jakob@westhoffswelt.de>
2021-07-02 20:23:36 +0200

    Changed Color visibility to be public

--- src/canvas.rs
+++ src/canvas.rs
@@ -4,10 +4,10 @@ use std::ops::{Add, Mul, Sub};
 use super::util::*;
 
 #[derive(Debug)]
-struct Color {
-  red: f64,
-  green: f64,
-  blue: f64,
+pub struct Color {
+  pub red: f64,
+  pub green: f64,
+  pub blue: f64,
 }
 
 impl Color {

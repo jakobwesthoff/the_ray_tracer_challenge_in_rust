a76ddf3b78149077fd09b9424e2137876666ee3c
Jakob Westhoff <jakob@westhoffswelt.de>
2021-07-04 17:45:09 +0200

    Implement assert_fuzzy_ne macro for inequaility assertion

--- src/fuzzy_eq.rs
+++ src/fuzzy_eq.rs
@@ -16,6 +16,7 @@ impl FuzzyEq<f64> for f64 {
 
 // Not really sure what I am doing here, as I don't have a great understanding of macros yet.
 // Feel free to fix or enhance in the future.
+// @TODO: Check if we can ensure more explicitly the two operands implement the `FuzzyEq` trait
 #[macro_export]
 macro_rules! assert_fuzzy_eq {
     ($left:expr, $right:expr $(,)?) => ({
@@ -23,6 +24,19 @@ macro_rules! assert_fuzzy_eq {
             (left_val, right_val) => {
                 if left_val.fuzzy_ne(right_val) {
                     panic!("asserting fuzzy equality. {:?} is not fuzzy equal to {:?}", left_val, right_val);
+                }
+            }
+        }
+    });
+}
+
+#[macro_export]
+macro_rules! assert_fuzzy_ne {
+    ($left:expr, $right:expr $(,)?) => ({
+        match (&$left, &$right) {
+            (left_val, right_val) => {
+                if left_val.fuzzy_eq(right_val) {
+                    panic!("asserting fuzzy in-equality. {:?} is fuzzy equal to {:?}", left_val, right_val);
                 }
             }
         }

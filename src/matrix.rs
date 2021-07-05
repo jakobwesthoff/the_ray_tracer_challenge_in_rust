880015db3ccf41e11781b42c6d2cd5d3ad79a322
Jakob Westhoff <jakob@westhoffswelt.de>
2021-07-05 11:26:09 +0200

    Refactored Matrices to utilize structs instead of raw arrays

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,8 +1,70 @@
 use crate::fuzzy_eq::*;
+use std::convert::From;
+use std::ops::Index;
 
-pub type Matrix2f = [[f64; 2]; 2];
-pub type Matrix3f = [[f64; 3]; 3];
-pub type Matrix4f = [[f64; 4]; 4];
+type Matrix2fArrayRow = [f64; 2];
+type Matrix2fArray = [Matrix2fArrayRow; 2];
+type Matrix3fArrayRow = [f64; 3];
+type Matrix3fArray = [Matrix3fArrayRow; 3];
+type Matrix4fArrayRow = [f64; 4];
+type Matrix4fArray = [Matrix4fArrayRow; 4];
+
+#[derive(Debug)]
+pub struct Matrix2f {
+  data: Matrix2fArray,
+}
+
+#[derive(Debug)]
+pub struct Matrix3f {
+  data: Matrix3fArray,
+}
+
+#[derive(Debug)]
+pub struct Matrix4f {
+  data: Matrix4fArray,
+}
+
+impl From<Matrix2fArray> for Matrix2f {
+  fn from(data: Matrix2fArray) -> Self {
+    Matrix2f { data }
+  }
+}
+
+impl From<Matrix3fArray> for Matrix3f {
+  fn from(data: Matrix3fArray) -> Self {
+    Matrix3f { data }
+  }
+}
+
+impl From<Matrix4fArray> for Matrix4f {
+  fn from(data: Matrix4fArray) -> Self {
+    Matrix4f { data }
+  }
+}
+
+impl Index<usize> for Matrix2f {
+  type Output = Matrix2fArrayRow;
+
+  fn index(&self, index: usize) -> &Self::Output {
+    &self.data[index]
+  }
+}
+
+impl Index<usize> for Matrix3f {
+  type Output = Matrix3fArrayRow;
+
+  fn index(&self, index: usize) -> &Self::Output {
+    &self.data[index]
+  }
+}
+
+impl Index<usize> for Matrix4f {
+  type Output = Matrix4fArrayRow;
+
+  fn index(&self, index: usize) -> &Self::Output {
+    &self.data[index]
+  }
+}
 
 impl FuzzyEq<Matrix2f> for Matrix2f {
   fn fuzzy_eq(&self, other: &Matrix2f) -> bool {
@@ -54,12 +116,12 @@ mod tests {
 
   #[test]
   fn constructing_and_inspecting_a_4x4_matrix() {
-    let m: Matrix4f = [
+    let m = Matrix4f::from([
       [1.0, 2.0, 3.0, 4.0],
       [5.5, 6.5, 7.5, 8.5],
       [9.0, 10.0, 11.0, 12.0],
       [13.5, 14.5, 15.5, 16.5],
-    ];
+    ]);
 
     assert_eq!(m[0][0], 1.0);
     assert_eq!(m[0][1], 2.0);
@@ -81,7 +143,7 @@ mod tests {
 
   #[test]
   fn a_2x2_matrix_ought_to_be_representable() {
-    let m: Matrix2f = [[-3.0, 5.0], [1.0, -2.0]];
+    let m = Matrix2f::from([[-3.0, 5.0], [1.0, -2.0]]);
 
     assert_eq!(m[0][0], -3.0);
     assert_eq!(m[0][1], 5.0);
@@ -91,7 +153,7 @@ mod tests {
 
   #[test]
   fn a_3x3_matrix_ought_to_be_representable() {
-    let m: Matrix3f = [[-3.0, 5.0, 0.0], [1.0, -2.0, -7.0], [0.0, 1.0, 1.0]];
+    let m = Matrix3f::from([[-3.0, 5.0, 0.0], [1.0, -2.0, -7.0], [0.0, 1.0, 1.0]]);
 
     assert_eq!(m[0][0], -3.0);
     assert_eq!(m[0][1], 5.0);
@@ -106,102 +168,103 @@ mod tests {
 
   #[test]
   fn matrix_equality_with_identical_2x2_matrices() {
-    let m1 = [[0.123456789, 1.0], [2.0, 3.0]];
-    let m2 = [[0.123456789, 1.0], [2.0, 3.0]];
+    let m1 = Matrix2f::from([[0.123456789, 1.0], [2.0, 3.0]]);
+    let m2 = Matrix2f::from([[0.123456789, 1.0], [2.0, 3.0]]);
 
     assert_fuzzy_eq!(m1, m2);
   }
 
   #[test]
   fn matrix_equality_with_almost_identical_2x2_matrices() {
-    let m1 = [[0.123456789, 1.0], [2.0, 3.0]];
-    let m2 = [[0.123456780, 1.0], [2.0, 3.0]];
+    let m1 = Matrix2f::from([[0.123456789, 1.0], [2.0, 3.0]]);
+    let m2 = Matrix2f::from([[0.123456780, 1.0], [2.0, 3.0]]);
 
     assert_fuzzy_eq!(m1, m2);
   }
 
   #[test]
   fn matrix_equality_with_identical_3x3_matrices() {
-    let m1 = [
+    let m1= Matrix3f::from([
       [0.123456789, 1.0, 2.0],
       [2.0, 3.0, 4.0],
       [5.0, 6.0, 7.7777777777777777],
-    ];
-    let m2 = [
+    ]);
+
+    let m2 = Matrix3f::from([
       [0.123456789, 1.0, 2.0],
       [2.0, 3.0, 4.0],
       [5.0, 6.0, 7.7777777777777777],
-    ];
+    ]);
 
     assert_fuzzy_eq!(m1, m2);
   }
 
   #[test]
   fn matrix_equality_with_almost_identical_3x3_matrices() {
-    let m1 = [
+    let m1 = Matrix3f::from([
       [0.123456789, 1.0, 2.0],
       [2.0, 3.0, 4.0],
       [5.0, 6.0, 7.7777777777777777],
-    ];
-    let m2 = [
+    ]);
+    let m2 = Matrix3f::from([
       [0.123456780, 1.0, 2.0],
       [2.0, 3.0, 4.0],
       [5.0, 6.0, 7.7777777777777],
-    ];
+    ]);
 
     assert_fuzzy_eq!(m1, m2);
   }
 
   #[test]
   fn matrix_equality_with_identical_4x4_matrices() {
-    let m1 = [
+    let m1 = Matrix4f::from([
       [0.123456789, 1.0, 2.0, 42.0],
       [2.0, 3.0, 4.0, -42.0],
       [5.0, 6.0, 7.7777777777777777, 23.5],
       [0.0, 0.0, 0.0, 1.0],
-    ];
-    let m2 = [
+    ]);
+    let m2 = Matrix4f::from([
       [0.123456789, 1.0, 2.0, 42.0],
       [2.0, 3.0, 4.0, -42.0],
       [5.0, 6.0, 7.7777777777777777, 23.5],
       [0.0, 0.0, 0.0, 1.0],
-    ];
+    ]);
 
     assert_fuzzy_eq!(m1, m2);
   }
 
   #[test]
   fn matrix_equality_with_almost_identical_4x4_matrices() {
-    let m1 = [
+    let m1 = Matrix4f::from([
       [0.123456789, 1.0, 2.0, 42.0],
       [2.0, 3.0, 4.0, -42.0],
       [5.0, 6.0, 7.7777777777777777, 23.5],
       [0.0, 0.0, 0.0, 1.0000000000001],
-    ];
-    let m2 = [
+    ]);
+    let m2 = Matrix4f::from([
       [0.123456789, 1.0, 2.0, 42.0],
       [2.0, 3.0, 4.0, -42.0],
       [5.0, 6.0, 7.7777777777777, 23.5],
       [0.0, 0.0, 0.0, 1.0],
-    ];
+    ]);
 
     assert_fuzzy_eq!(m1, m2);
   }
 
   #[test]
   fn matrix_inequality_with_non_identical_4x4_matrices() {
-    let m1 = [
+    let m1 = Matrix4f::from([
       [0.123456789, 1.0, 2.0, 42.0],
       [2.0, 3.0, 4.0, -42.0],
       [5.0, 6.0, 7.7777777777777777, 23.5],
       [0.0, 0.0, 0.0, 1.0],
-    ];
-    let m2 = [
+    ]);
+    let m2 = Matrix4f::from([
       [0.123456789, 1.0, 2.0, 42.0],
       [2.0, 3.0, 4.0, -42.0],
       [5.0, 6.0, 7.7777777777777777, 23.5],
       [0.0, 0.0, 0.0, 2.0],
-    ];
+    ]);
 
     assert_fuzzy_ne!(m1, m2);
   }

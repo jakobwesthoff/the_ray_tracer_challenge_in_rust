45fb8dba02650ee4ec11865a90b140b79d4346d9
Jakob Westhoff <jakob@westhoffswelt.de>
2021-11-10 18:21:16 +0100

    Implemented multi range support as well as negativ interpolation

--- src/animator.rs
+++ src/animator.rs
@@ -1,15 +1,18 @@
+use core::slice;
+use std::cmp::min;
+
 use crate::F;
 
 pub struct LinearScale {
   domain: (F, F),
-  range: (F, F),
+  range: Vec<F>,
 }
 
 impl LinearScale {
   pub fn new() -> Self {
     Self {
       domain: (0.0, 100.0),
-      range: (0.0, 1.0),
+      range: vec![0.0, 1.0],
     }
   }
 
@@ -18,18 +21,55 @@ impl LinearScale {
     self
   }
 
-  pub fn with_range(mut self, start: F, end: F) -> Self {
-    self.range = (start, end);
+  pub fn with_range(mut self, range: Vec<F>) -> Self {
+    self.range = range;
     self
   }
 
   pub fn scale(&self, input: F) -> F {
     let clamped_input = input.clamp(self.domain.0, self.domain.1);
-    let offsetted_input = clamped_input - self.domain.0;
-    let normalized_input = offsetted_input / (self.domain.1 - self.domain.0);
-    let offseted_output = normalized_input * (self.range.1 - self.range.0);
-    let output = self.range.0 + offseted_output;
-    output.clamp(self.range.0, self.range.1)
+
+    let normalized_input = self.normalize(clamped_input);
+    let output = self.interpolate(normalized_input);
+
+    output.clamp(self.minimum_of_range(), self.maximum_of_range())
+  }
+
+  fn minimum_of_range(&self) -> F {
+    let mut minimum = F::INFINITY;
+    for value in self.range.iter() {
+      minimum = minimum.min(*value);
+    }
+
+    minimum
+  }
+
+  fn maximum_of_range(&self) -> F {
+    let mut maximum = -F::INFINITY;
+    for value in self.range.iter() {
+      maximum = maximum.max(*value);
+    }
+    maximum
+  }
+
+  fn normalize(&self, input: F) -> F {
+    let offsetted_input = input - self.domain.0;
+    offsetted_input / (self.domain.1 - self.domain.0)
+  }
+
+  fn interpolate(&self, normalized_input: F) -> F {
+    let slice_count = self.range.len() - 1;
+    let slice_index = (normalized_input * slice_count as F).floor() as usize;
+    let slice_normalized_input = normalized_input * slice_count as F - slice_index as F;
+
+    let offseted_output =
+      slice_normalized_input * (self.range[slice_index + 1] - self.range[slice_index]).abs();
+
+    if self.range[slice_index] > self.range[slice_index + 1] {
+      return self.range[slice_index] - offseted_output;
+    } else {
+      return self.range[slice_index] + offseted_output;
+    }
   }
 }
 
